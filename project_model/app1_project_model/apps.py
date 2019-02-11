32cb5c3b8c889a7402e2ecc666073e53040bf9c1
kiranahp <kirana@alphatech.id>
2019-02-11 12:40:44 +0700

    project_django

---
+++ project_model/app1_project_model/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class App1ProjectModelConfig(AppConfig):
+    name = 'app1_project_model'
---
+++ project_views/app1_project_views/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class App1ProjectViewsConfig(AppConfig):
+    name = 'app1_project_views'
